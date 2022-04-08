344da205bd9abc4c8a6309db0f863bf3b8627fc0
Franco_Barrios <soyelfrancoster@gmail.com>
2022-04-08 15:57:29 -0300

    Sacada ventana

--- src/juego/Juego.java
+++ src/juego/Juego.java
@@ -1,8 +1,27 @@
 package juego;
 
+import java.awt.*;
+import javax.swing.*;
+
 public class Juego {
 
-   
-
-    
+    private static void createWindow() {
+        JFrame frame = new JFrame("Jueguito random");
+        
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        JLabel HPEnem = new JLabel("HP de enemigo", SwingConstants.CENTER);
+        HPEnem.setPreferredSize(new Dimension(300,100));
+        
+        frame.getContentPane().add(HPEnem, BorderLayout.CENTER);
+        
+        frame.setLocationRelativeTo(null);
+        
+        frame.pack();
+        
+        frame.setVisible(true);
+    }
+    public static void main(String[] args) {
+        createWindow();
+    }
 }
--- src/juego/Personaje.java
+++ src/juego/Personaje.java
@@ -6,13 +6,14 @@ public class Personaje {
 
     public double defensaP = 10;
     public int hp = 0;
-    public int cooldown = 0;
+
     public int cantidad = 2;
+    long lastAttack = 0;
 
     Personaje() {
         salud();
         defensa();
-        
+
     }
 
     public static int getRandomInteger(int maximo, int minimo) {
@@ -48,58 +49,65 @@ public class Personaje {
                 dañoEntero *= 2;
                 System.out.println("Critico!");
                 enemigo.hp = enemigo.hp - dañoEntero;
-                System.out.println("Daño: "+dañoEntero);
+                System.out.println("Daño: " + dañoEntero);
 
             } else {
                 enemigo.hp = enemigo.hp - dañoEntero;
             }
             System.out.println("No critico");
-            System.out.println("Daño: "+dañoEntero);
+            System.out.println("Daño: " + dañoEntero);
 
         }
     }
-    
-    public void ataqueDoble(Personaje personaje, Personaje persona) {
-        if (persona.cooldown == 0){
-            double ataque = getRandomInteger(20, 10);
-            persona.cooldown +=3;
-        double chanceCritico = getRandomInteger(10, 2);
 
-        double daño = ataque + (ataque*2 * Math.random());
-        int dañoEntero = (int) Math.round(daño);
-        double critico = chanceCritico * Math.random();
+    public void ataqueDoble(Personaje heroe, Personaje enemigo) {
+
+        if (cooldown() == true) {
+
+            double ataque = getRandomInteger(20, 10);
+
+            double chanceCritico = getRandomInteger(10, 2);
+
+            double daño = ataque + (ataque * 2 * Math.random());
+            int dañoEntero = (int) Math.round(daño);
+            double critico = chanceCritico * Math.random();
             if (critico > 5.0) {
                 dañoEntero *= 2;
                 System.out.println("DOBLE CRITICO!");
-                personaje.hp = personaje.hp - dañoEntero;
+                enemigo.hp = enemigo.hp - dañoEntero;
 
             } else {
-                personaje.hp = personaje.hp - dañoEntero;
+                enemigo.hp = enemigo.hp - dañoEntero;
             }
             System.out.println("Golpe doble!");
-        } else System.out.println("Habilidad en recarga: " + persona.cooldown + " turnos");
-        
+        } else {
+            System.out.println("Habilidad en recarga: ");
+        }
 
-        }
-    
-    public void cooldown(Personaje personaje) {
-        if (personaje.cooldown != 0){
-            personaje.cooldown --;
-        }
     }
-    
-     public static void dañoAtaque(Personaje heroe, Personaje enemigo) {
+
+    public boolean cooldown() {
+
+        long cooldownTime = 10000; // 10000 milliseconds
+        long time = System.currentTimeMillis();
+        if (time > lastAttack + cooldownTime) {
+            lastAttack = time;
+            return true;
+        } else {
+            System.out.println("Tiempo: " + (10 - (time - lastAttack) / 1000) + " segundos");
+        }
+        return false;
+    }
+
+    public static void dañoAtaque(Personaje heroe, Personaje enemigo) {
         heroe.ataque(enemigo);
-        
-        }
-    
-    public static void sanar(Personaje personaje){
-        personaje.curar(personaje);
-        
-    }
-    
 
-    
+    }
+
+    public static void sanar(Personaje personaje) {
+        personaje.curar(personaje);
+
+    }
 
     public int defensa() {
 
